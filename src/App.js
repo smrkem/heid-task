b0b4b6640f1071ae1bf21da2e6e30bbda864c681
matt-smrke <matthew.smrke@empire.ca>
2018-10-23 03:13:57 -0400

    adds header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import ExperimentManager from './components/ExperimentManager/ExperimentManager'
+import Header from './components/Header/Header';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>HEID Task</h1>
-        </header>
+        <Header />
         <div className="App-body">
           <ExperimentManager />        
         </div>
