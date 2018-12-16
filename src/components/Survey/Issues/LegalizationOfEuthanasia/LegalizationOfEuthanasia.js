29f71049aa68eb646e75cea1b5cd83fd79d3de74
matt-smrke <matthew.smrke@empire.ca>
2018-12-16 16:21:28 -0500

    initial lissues

---
+++ src/components/Survey/Issues/LegalizationOfEuthanasia/LegalizationOfEuthanasia.js
@@ -0,0 +1,16 @@
+import imgSrc from './issue-metoo.jpg';
+
+export default {
+  title: "Legalization of Marijuana",
+  problem_statement: "",
+  image_src: imgSrc,
+  description: [
+
+  ],
+  pros: [
+
+  ],
+  cons: [
+
+  ]
+}
