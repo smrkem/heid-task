be72570f91955aeee79cd427f6e4750359cd276f
matt-smrke <matthew.smrke@empire.ca>
2019-01-18 16:44:11 -0500

    adds image preloading

--- src/App.js
+++ src/App.js
@@ -3,7 +3,26 @@ import './App.css';
 import TaskManager from './components/TaskManager/TaskManager'
 import Header from './components/Header/Header';
 
+// preload images
+import figurePondering from './components/Survey/components/FigurePondering/figure_pondering.png'
+import issues from './components/Survey/Issues/Issues'
 
+      
+const PreloadedImages = (props) => {
+  // console.log('fP:', figurePondering);
+  const inlineImages = issues.map(iss => iss.image_src);
+  inlineImages.push(figurePondering);
+  // console.log('images', inlineImages);
+  // console.log('images', inlineImages);
+
+  return (
+    <div id="preloaded_images">
+      { inlineImages.map(imageSrc => (
+        <img src={imageSrc} key={imageSrc} alt="preload" />
+      ))}
+    </div>
+  )
+}
 class App extends Component {
   render() {
     return (
@@ -12,6 +31,7 @@ class App extends Component {
         <div className="App-body">
           <TaskManager />        
         </div>
+        <PreloadedImages />
       </div>
     );
   }
